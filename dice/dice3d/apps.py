7f59fd40369b33d6e8c7c853df70d60fac94dea5
Elladan81 <heladan@hotmail.fr>
2020-03-22 23:16:55 +0100

    Initial commit

---
+++ dice/dice3d/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Dice3DConfig(AppConfig):
+    name = 'dice3d'
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Mickael\PycharmProjects\3d_dice test\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
